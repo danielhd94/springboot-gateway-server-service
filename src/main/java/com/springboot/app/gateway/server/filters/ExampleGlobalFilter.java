2e435d82c6744c065177fe96cdd946f6bc4a30be
Daniel Hernandez <danielhd94@hotmail.com>
2024-03-21 19:01:45 -0700

    Add order filters

--- src/main/java/com/springboot/app/gateway/server/filters/ExampleGlobalFilter.java
+++ src/main/java/com/springboot/app/gateway/server/filters/ExampleGlobalFilter.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class ExampleGlobalFilter implements GlobalFilter {
+public class ExampleGlobalFilter implements GlobalFilter, Ordered {
 	
 	private final Logger logger = LoggerFactory.getLogger(ExampleGlobalFilter.class);
 
@@ -35,6 +36,11 @@ public class ExampleGlobalFilter implements GlobalFilter {
 			 exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
 			 exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
+	}
+
+	@Override
+	public int getOrder() {
+		return -1;
 	}
 
 }
