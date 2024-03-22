ade725a9a449cb17cc251f16bb16485f950bf287
Daniel Hernandez <danielhd94@hotmail.com>
2024-03-21 18:21:45 -0700

    first commit

---
+++ src/main/java/com/springboot/app/gateway/server/SpringbootGatewayServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.gateway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGatewayServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGatewayServerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/gateway/server/SpringbootGatewayServerServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springboot.app.gateway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootTest
+class SpringbootGatewayServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
