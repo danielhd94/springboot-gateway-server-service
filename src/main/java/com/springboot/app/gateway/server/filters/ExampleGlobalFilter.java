937b411adf5e5ebb7e1dd9155bcadc43e0e15ad0
Daniel Hernandez <danielhd94@hotmail.com>
2024-03-25 18:21:59 -0700

    Add factory filter methods

--- src/main/java/com/springboot/app/gateway/server/filters/ExampleGlobalFilter.java
+++ src/main/java/com/springboot/app/gateway/server/filters/ExampleGlobalFilter.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -16,31 +15,28 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class ExampleGlobalFilter implements GlobalFilter, Ordered {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(ExampleGlobalFilter.class);
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		logger.info("Ejecutando filtro pre");
 		exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
-		
-		
+
 		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-			 logger.info("Ejecutando filtro post");
-			 
-			 Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(value -> {
-				 exchange.getResponse().getHeaders().add("token", value);
-			 });
-			 
-			 
-			 exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
-			 exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+			logger.info("Ejecutando filtro post");
+
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(value -> {
+				exchange.getResponse().getHeaders().add("token", value);
+			});
+
+			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
 		}));
 	}
 
 	@Override
 	public int getOrder() {
-		return -1;
+		return 1;
 	}
 
 }
--- src/main/java/com/springboot/app/gateway/server/filters/factory/ExampleGatewayFilterFactory.java
+++ src/main/java/com/springboot/app/gateway/server/filters/factory/ExampleGatewayFilterFactory.java
@@ -1,6 +1,10 @@
 package com.springboot.app.gateway.server.filters.factory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.constraints.NotBlank;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +12,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
@@ -17,34 +22,50 @@ public class ExampleGatewayFilterFactory
 
 	private final Logger logger = LoggerFactory.getLogger(ExampleGatewayFilterFactory.class);
 
-	
-
 	public ExampleGatewayFilterFactory() {
 		super(Configuration.class);
 	}
 
-
 	@Override
 	public GatewayFilter apply(Configuration config) {
 		return (exchange, chain) -> {
-
-			logger.info("Ejecutando pre gateway filter factory: " + config.message);
+			logPreFilterExecution(config);
 			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-
-				Optional.ofNullable(config.cookieValue).ifPresent((cookie) -> {
-					exchange.getResponse().addCookie(ResponseCookie.from(config.cookieName, cookie).build());
-				});
-				logger.info("Ejecutando post gateway filter factory: " + config.message);
+				addCookieToResponse(exchange, config);
+				logPostFilterExecution(config);
 			}));
 		};
 	}
-	
-	
-	public class Configuration {
 
+	private void logPreFilterExecution(Configuration config) {
+		logger.info("Ejecutando pre gateway filter factory: {}", config.getMessage());
+	}
+
+	private void logPostFilterExecution(Configuration config) {
+		logger.info("Ejecutando post gateway filter factory: {}", config.getMessage());
+	}
+
+	private void addCookieToResponse(ServerWebExchange exchange, Configuration config) {
+		Optional.ofNullable(config.getCookieValue()).ifPresent(cookieValue -> {
+			exchange.getResponse().addCookie(ResponseCookie.from(config.getCookieName(), cookieValue).build());
+		});
+	}
+
+	@Override
+	public String name() {
+		return "cookies";
+	}
+
+	@Override
+	public List<String> shortcutFieldOrder() {
+		return Arrays.asList("message", "cookieName", "cookieValue");
+	}
+
+	public static class Configuration {
+		@NotBlank
 		private String message;
-		private String cookieValue;
 		private String cookieName;
+		private String cookieValue;
 
 		public String getMessage() {
 			return message;
@@ -52,14 +73,6 @@ public class ExampleGatewayFilterFactory
 
 		public void setMessage(String message) {
 			this.message = message;
-		}
-
-		public String getCookieValue() {
-			return cookieValue;
-		}
-
-		public void setCookieValue(String cookieValue) {
-			this.cookieValue = cookieValue;
 		}
 
 		public String getCookieName() {
@@ -70,6 +83,12 @@ public class ExampleGatewayFilterFactory
 			this.cookieName = cookieName;
 		}
 
-	}
+		public String getCookieValue() {
+			return cookieValue;
+		}
 
+		public void setCookieValue(String cookieValue) {
+			this.cookieValue = cookieValue;
+		}
+	}
 }
